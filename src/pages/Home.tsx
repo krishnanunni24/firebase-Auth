a6482de15b5e149d39cfb651e79ba2e4dc64ba22
Krishnanunni.S <krishnanunni2424@gmail.com>
2023-09-15 17:30:26 +0530

    home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,13 @@ import Header from "../components/Header/Header"
 
 function Home() {
   return (
+    <>
     <Header/>
+    <div className="flex justify-center items-center h-auto text-lg">
+      HOME PAGE
+    </div>
+    
+    </>
   )
 }
 
